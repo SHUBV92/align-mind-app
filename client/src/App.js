ae3002c7e127f7afaa3063a18bf83d2b901e5693
SHUBV92 <shubinder92@gmail.com>
2020-11-18 23:35:00 +0000

    Created Input Component

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,10 @@
 import React from "react"
+import Input from "./component/input"
 
 const App = () => {
   return (
-    <div className="App">     
+    <div className="App">    
+    <h1>Align Mind</h1>
     </div>
   );
 }
